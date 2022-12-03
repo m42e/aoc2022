94f207926bd7b15ee79308187603b08df93b9026
Matthias Bilger <matthias@bilger.info>
2022-12-03 05:55:51 +0100

    day02

--- day02/module.py
+++ day02/module.py
@@ -9,90 +9,88 @@ import functools
 
 
 def pw(line):
-    return line.strip().split(' ')
+    return line.strip().split(" ")
+
 
 # group_by_empty_line
 
+
 def p1():
     inp = get_input(pw)
-    win = [
-        ['A', 'Z'],
-        ['B', 'X'],
-        ['C', 'Y'],
-    ]
-    draw = [
-        ['A', 'X'],
-        ['B', 'Y'],
-        ['C', 'Z'],
-    ]
+    results = [0, 0]
+    elv, me = 0, 1
+
+    fight = {
+        ("A", "Z"): (elv, elv),  # elv wins
+        ("B", "X"): (elv, elv),  # elv wins
+        ("C", "Y"): (elv, elv),  # elv wins
+        ("A", "X"): (elv, me),  # draw
+        ("B", "Y"): (elv, me),  # draw
+        ("C", "Z"): (elv, me),  # draw
+        ("A", "Y"): (me, me),  # I win
+        ("B", "Z"): (me, me),  # I win
+        ("C", "X"): (me, me),  # I win
+    }
     extra = {
-        'A': 1,
-        'B': 2,
-        'C': 3,
-        'X': 1,
-        'Y': 2,
-        'Z': 3,
+        "A": 1,
+        "B": 2,
+        "C": 3,
+        "X": 1,
+        "Y": 2,
+        "Z": 3,
     }
     points_e = 0
     points_me = 0
-    for sample in inp:
-        print(sample)
-        if sample in win:
-            points_e += 6
-            points_e += extra[sample[0]]
-            points_me += extra[sample[1]]
-        elif sample not in draw:
-            points_me += 6
-            points_me += extra[sample[1]]
-            points_e += extra[sample[0]]
-        else:
-            points_me += 3
-            points_me += extra[sample[1]]
-            points_e += 3
-            points_e += extra[sample[1]]
-        print(points_me)
+    for rps1, rps2 in inp:
+        one, two = fight[(rps1, rps2)]
+        results[one] += 3
+        results[two] += 3
+        results[elv] += extra[rps1]
+        results[me] += extra[rps2]
+
+    print(results[me])
     return inp
 
 
 def p2(inp):
     lose = {
-        'A': 'Z',
-        'B': 'X',
-        'C': 'Y',
+        "A": "Z",
+        "B": "X",
+        "C": "Y",
     }
     win = {
-        'A': 'Y',
-        'B': 'Z',
-        'C': 'X',
+        "A": "Y",
+        "B": "Z",
+        "C": "X",
     }
     draw = {
-        'A': 'X',
-        'B': 'Y',
-        'C': 'Z',
+        "A": "X",
+        "B": "Y",
+        "C": "Z",
     }
     extra = {
-        'A': 1,
-        'B': 2,
-        'C': 3,
-        'X': 1,
-        'Y': 2,
-        'Z': 3,
+        "A": 1,
+        "B": 2,
+        "C": 3,
+        "X": 1,
+        "Y": 2,
+        "Z": 3,
     }
     points_e = 0
     points_me = 0
     for sample in inp:
-        if sample[1] == 'X':
+        if sample[1] == "X":
             me = lose[sample[0]]
             points_e += 6
             points_e += extra[sample[0]]
             points_me += extra[me]
-        elif sample[1] == 'Y':
+        elif sample[1] == "Y":
             me = draw[sample[0]]
             points_me += 3
             points_me += extra[me]
             points_e += 3
             points_e += extra[sample[0]]
-        elif sample[1] == 'Z':
+        elif sample[1] == "Z":
             me = win[sample[0]]
             points_me += 6
             points_me += extra[me]
